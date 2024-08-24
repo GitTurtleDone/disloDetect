e439f388adadd4bb28357880eec98c5867985a94
GitTurtleDone <giang2110@gmail.com>
2024-08-24 12:30:03 +1200

    Used MathJax in React child components

--- dislodetect_be/obj/Debug/net8.0/dislodetect_be.AssemblyInfo.cs
+++ dislodetect_be/obj/Debug/net8.0/dislodetect_be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c1b3c3a495cc963bda5a518deea5e94a38d34d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f808eba35f2fad9c25c1f6ea5ffa28130acb1b8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
