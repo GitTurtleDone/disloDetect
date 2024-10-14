e9c810a8e75a0ac0bc453c91c55c78d0c55b4f26
GitTurtleDone <giang2110@gmail.com>
2024-10-15 06:57:51 +1300

    Changed the weights for Roboflow and Ultralytics predictions

--- dislodetect_be/obj/Debug/net8.0/dislodetect_be.AssemblyInfo.cs
+++ dislodetect_be/obj/Debug/net8.0/dislodetect_be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f3656faa83764d59e84ddbe3db520bfa6826709")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ca5a722cfcce6de82f001fca68e03d089f74a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
