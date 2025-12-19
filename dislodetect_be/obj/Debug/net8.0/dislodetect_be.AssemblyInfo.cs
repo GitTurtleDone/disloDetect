b3c2f98501f5aec663e2e3b4d16307dad8f95c4a
GitTurtleDone <giang2110@gmail.com>
2025-12-20 11:54:53 +1300

    Addede a README.md file

--- dislodetect_be/obj/Debug/net8.0/dislodetect_be.AssemblyInfo.cs
+++ dislodetect_be/obj/Debug/net8.0/dislodetect_be.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73a64ce59d606ef4efc1cc344ef561f381957363")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8a624c4137ef6fce35c0242bcd3070823932fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dislodetect_be")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
